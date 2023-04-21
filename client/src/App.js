30348c303ffdff8cc2a3c2721949594dffa53d61
Ryguy <ryan70297@gmail.com>
2023-04-20 23:22:01 -0300

    fixed for heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
     
   useEffect(() => {
    const getUser = async () => {
-    fetch('https://newbrunswick-dev.herokuapp.com//auth/login/success', {
+    fetch('https://newbrunswick-dev.herokuapp.com/auth/login/success', {
       method: 'GET',
       credentials: 'include',
       headers: { Accept: 'application/json', 'Content-Type': 'application/json', 'Access-Control-Allow-Credentials': true}
